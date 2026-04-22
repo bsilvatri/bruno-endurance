9ce08bec478d734fb87ff69f85af2ae534de0123
bsilvatri <bsilvatri@gmail.com>
2026-04-22 20:54:15 -0300

    trigger redeploy

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,1 @@
-import{defineConfig}from'vite';import react from'@vitejs/plugin-react';export default defineConfig({plugins:[react()]}) // v1776808152623
+import{defineConfig}from'vite';import react from'@vitejs/plugin-react';export default defineConfig({plugins:[react()]})// 1776902055034
